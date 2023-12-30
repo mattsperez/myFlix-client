19c5b1dea3134213f4d85ab10f8f1bee01eff58f
mattsperez <131983739+mattsperez@users.noreply.github.com>
2023-12-30 10:31:57 -0600

    Create movie-view.jsx

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,107 @@
+import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import { Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+
+import React from 'react';
+
+export const MovieView = ({ setUser, user, movies, token }) => {
+	const { Title } = useParams();
+	const movie = movies.find((m) => m.Title === Title);
+
+	const [isFavorite, setIsFavorite] = useState(
+		user.FavoriteMovie.includes(movie._id)
+	);
+
+	const addFavoriteMovie = () => {
+		fetch(
+			'https://mi-movie-app-6566ce5d2cb3.herokuapp.com/users/' +
+				user.Username +
+				'/movies/' +
+				movie._id,
+			{
+				method: 'POST',
+				headers: {
+					'Content-type': 'application/json',
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		)
+			.then((response) => response.json())
+
+			.then((data) => {
+				localStorage.setItem('user', JSON.stringify(data));
+				setUser(user);
+				setIsFavorite(true);
+				alert('Movie added!');
+			});
+	};
+
+	const removeFavoriteMovie = () => {
+		data = fetch(
+			'https://mi-movie-app-6566ce5d2cb3.herokuapp.com/users/' +
+				user.Username +
+				'/movies/' +
+				movie._id,
+			{
+				method: 'DELETE',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				localStorage.setItem('user', JSON.stringify(data));
+				setUser(data);
+				setIsFavorite(false);
+				alert('movie removed!');
+			})
+			.catch((e) => {
+				alert('Something went wrong!');
+			});
+	};
+
+	return (
+		<Card bg={'light'} border='dark' style={{ width: '55rem' }}>
+			<Row>
+				<Col>
+					<Card.Img variant='top' src={movie.ImagePath} />
+				</Col>
+				<Col>
+					<Card.Body>
+						<Card.Title>{movie.Title}</Card.Title>
+						<Card.Text>Description: {movie.Description}</Card.Text>
+						<Card.Text>Director: {movie.Director.Name}</Card.Text>
+						<Card.Text>Bio: {movie.Director.Bio}</Card.Text>
+						<Card.Text>Genre: {movie.Genre.Name}</Card.Text>
+						<Card.Text>Description: {movie.Genre.Description}</Card.Text>
+						<Row>
+							<Col>
+								{isFavorite ? (
+									<Button variant='danger' onClick={removeFavoriteMovie}>
+										Remove Favorite
+									</Button>
+								) : (
+									<Button variant='success' onClick={addFavoriteMovie}>
+										Add Favorite
+									</Button>
+								)}
+							</Col>
+							<Col>
+								<Link to={'/'}>
+									<Button className='back-button'>Back</Button>
+								</Link>
+							</Col>
+						</Row>
+					</Card.Body>
+				</Col>
+			</Row>
+		</Card>
+	);
+};
