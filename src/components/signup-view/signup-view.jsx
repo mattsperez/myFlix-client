8cca71de67832fd19bb8d853cb9e5e16cc1a3f1d
mattsperez <131983739+mattsperez@users.noreply.github.com>
2023-12-30 10:31:50 -0600

    Create signup-view.jsx

---
+++ src/components/signup-view/signup-view.jsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+
+import { Button } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+
+export const SignupView = () => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [email, setEmail] = useState('');
+	const [birthday, setBirthday] = useState('');
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		const data = {
+			Username: username,
+			Password: password,
+			Email: email,
+			Birthday: birthday,
+		};
+
+		fetch('https://mi-movie-app-6566ce5d2cb3.herokuapp.com/users', {
+			method: 'POST',
+			body: JSON.stringify(data),
+			headers: {
+				'Content-type': 'application/json',
+			},
+		}).then((response) => {
+			if (response.ok) {
+				alert('Signup successful!');
+				window.location.replace('/login');
+			} else {
+				alert('Signup failed!');
+			}
+		});
+	};
+
+	return (
+		<Col md={5}>
+			<Form className='form' onSubmit={handleSubmit}>
+				<Form.Group>
+					<Form.Label>Username:</Form.Label>
+					<Form.Control
+						type='text'
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+						required
+						minLength='3'
+					/>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Password:</Form.Label>
+					<Form.Control
+						type='password'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						required
+					/>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Email:</Form.Label>.
+					<Form.Control
+						type='email'
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						required
+					/>
+				</Form.Group>
+				<Form.Group>
+					<Form.Label>Birthday:</Form.Label>
+					<Form.Control
+						type='date'
+						value={birthday}
+						onChange={(e) => setBirthday(e.target.value)}
+						required
+					/>
+				</Form.Group>
+				<Button variant='primary' type='submit'>
+					Submit
+				</Button>
+			</Form>
+		</Col>
+	);
+};
